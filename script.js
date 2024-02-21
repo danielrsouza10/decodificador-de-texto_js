9abd16629a9456487bd24a77cee2d80ab6fbd93e
Daniel Souza <danielrsouza10@gmail.com>
2024-02-21 09:55:18 -0300

    funcao de descriptografar funcionando

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const cripButton = document.querySelector(".crip-button");
+const descripButton = document.querySelector(".descrip-button");
 const resultContainer = document.querySelector(".result");
 const resultSvgImage = document.querySelector(".svg-result");
-cripButton.addEventListener("click", inputCripografar);
+cripButton.addEventListener("click", inputCriptografar);
+descripButton.addEventListener("click", inputDescriptografar);
 
-function inputCripografar() {
+//capturar o input do usuário para criptografar
+function inputCriptografar() {
   const input = document.querySelector("#text-message");
   const textoCriptografado = criptografar(input);
-  resultContainer.appendChild(createResultDiv(textoCriptografado));
+  if (textoCriptografado != "") {
+    resultContainer.appendChild(createResultDiv(textoCriptografado));
+  }
+
   // console.log(textoCriptografado);
 }
 
+//capturar o input do usuário para descriptografar
+function inputDescriptografar() {
+  const input = document.querySelector("#text-message");
+  const textoCriptografado = descriptografar(input);
+  if (textoCriptografado != "") {
+    resultContainer.appendChild(createResultDiv(textoCriptografado));
+  }
+
+  // console.log(textoCriptografado);
+}
+
+//função para criptografar
 function criptografar(input) {
   const inputUsuario = input.value.split("");
   // console.log(inputUsuario);
@@ -29,6 +47,48 @@ function criptografar(input) {
   }
   result = inputUsuario.join("");
   // console.log(result);
+  return result;
+}
+
+//função para descriptografar
+function descriptografar(input) {
+  const inputUsuario = input.value.split("");
+  let result = "";
+  for (let i = 0; i < inputUsuario.length; i++) {
+    if (inputUsuario[i] == "a" && inputUsuario[i + 1] == "i") {
+      inputUsuario.splice(i, 2, "a");
+    } else if (
+      inputUsuario[i] == "e" &&
+      inputUsuario[i + 1] == "n" &&
+      inputUsuario[i + 2] == "t" &&
+      inputUsuario[i + 3] == "e" &&
+      inputUsuario[i + 4] == "r"
+    ) {
+      inputUsuario.splice(i, 5, "e");
+    } else if (
+      inputUsuario[i] == "i" &&
+      inputUsuario[i + 1] == "m" &&
+      inputUsuario[i + 2] == "e" &&
+      inputUsuario[i + 3] == "s"
+    ) {
+      inputUsuario.splice(i, 4, "i");
+    } else if (
+      inputUsuario[i] == "o" &&
+      inputUsuario[i + 1] == "m" &&
+      inputUsuario[i + 2] == "e" &&
+      inputUsuario[i + 3] == "r"
+    ) {
+      inputUsuario.splice(i, 4, "o");
+    } else if (
+      inputUsuario[i] == "u" &&
+      inputUsuario[i + 1] == "f" &&
+      inputUsuario[i + 2] == "a" &&
+      inputUsuario[i + 3] == "t"
+    ) {
+      inputUsuario.splice(i, 4, "u");
+    }
+  }
+  result = inputUsuario.join("");
   return result;
 }
 
